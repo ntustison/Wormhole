7a91e5500e2ae92f5fb9e362731e4327eda7d82c
Nick Tustison <ntustison@gmail.com>
2021-04-08 07:40:18 -0700

    BUG:  Recall/precision.

---
+++ Analysis/Evaluation/plotMsSeg2008Evaluation.R
@@ -0,0 +1,58 @@
+library( ANTsR )
+
+dataDirectory <- "/Users/ntustison/Data/WMH/MSSeg2008/"
+
+source( "./getEvaluationMeasures.R" )
+
+lesionFiles <- list.files( path = paste0( dataDirectory, "Nifti/" ), 
+    pattern = "lesion.nii.gz", recursive = TRUE, full.names = TRUE )
+
+
+measuresDataFrame <- data.frame()
+for( i in seq.int( length( lesionFiles ) ) )
+  {
+  resultsDirectory <- paste0( sub( "Nifti", "Processed", dirname( lesionFiles[i] ) ), "/" )
+  basePrefix <- sub( "lesion.nii.gz", "", basename( lesionFiles[i] ) )
+
+  cat( "Processing ", basePrefix, "(", i, "out of", length( lesionFiles ), ")\n" )    
+
+  reference <- antsImageRead( lesionFiles[i] )
+
+  ewDavid <- antsImageRead( paste0( resultsDirectory, basePrefix, "ew_david_wmh.nii.gz" ) )
+  ewDavid5 <- antsImageRead( paste0( resultsDirectory, basePrefix, "ew_david_wmh5.nii.gz" ) )
+  ewDavidT1 <- antsImageRead( paste0( resultsDirectory, basePrefix, "ew_david_t1only_wmh.nii.gz" ) )
+  ewDavid5T1 <- antsImageRead( paste0( resultsDirectory, basePrefix, "ew_david_t1only_wmh5.nii.gz" ) )
+  sysu <- antsImageRead( paste0( resultsDirectory, basePrefix, "sysu_wmh.nii.gz" ) )
+
+  segmentations <- list( ewDavid,
+                         ewDavid5,
+                         ewDavidT1,
+                         ewDavid5T1,
+                         sysu )
+  
+  segmentationNames <- c( "ewDavid", "ewDavid5", "ewDavidT1", "ewDavid5T1", "sysu" )
+ 
+  pb <- txtProgressBar( min = 0, max = length( segmentations ), style = 3 )
+  for( j in seq.int( length( segmentations ) ) )
+    {
+    setTxtProgressBar( pb, j )       
+    measures <- getEvaluationMeasures( segmentations[[j]], reference )
+    singleRow <- data.frame( Subject = basePrefix, Segmentation = segmentationNames[j], 
+                             DiceOverlap = measures$DiceOverlap, 
+                             HausdorffDistance = measures$HausdorffDistance,
+                             HausdorffAverageDistance = measures$HausdorffAverageDistance, 
+                             LogAbsoluteVolumetricDifference = measures$LogAbsoluteVolumetricDifference,
+                             Recall = measures$Recall,
+                             Precision = measures$Precision,
+                             F1 = measures$F1 )
+    if( nrow( measuresDataFrame ) == 0 )
+      {
+      measuresDataFrame <- singleRow    
+      } else {
+      measuresDataFrame <- rbind( measuresDataFrame, singleRow )
+      }  
+    write.csv( measuresDataFrame, file = "~/Desktop/measures.csv", row.names = FALSE, quote = FALSE )
+    }
+  cat( "\n" )  
+  }
+
